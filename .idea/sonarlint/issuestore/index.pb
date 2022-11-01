3621c640b858ea3a4c4d6dbe3ccc4d48b4a0d4c7
pasinimariano <marianolpasini@juacaxdev.com>
2022-11-01 20:10:14 -0300

    Check in has been added when getting all bookings, so that the admin can visalize the active guests on the current date. Also it was automated that when changing the booking status to check in the occupancy of room will be 1 and check out will we 0

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -19,8 +19,6 @@ K
 Procfile,b/c/bc3c68e880d1a01632bce79f69dd1387c7ba9c54
 Q
 !api/middlewares/data_validator.py,4/1/41d2bd64bec43f095eb27b770db892970c5be704
-H
-api/routes/categories.py,b/4/b436e99314c0af36df3bcf35d367ae268f0c03b3
 C
 api/routes/rooms.py,b/4/b4c378b5667a53aa44095c824bc33678cbfc0dc9
 D
@@ -43,12 +41,8 @@ R
 	config.py,1/a/1a1b9769344cb239f2e540e1e6ee1efa2d3034ea
 @
 requirements.txt,1/9/19359a61ae2446b51b549167b014da2fcf265768
-T
-$api/controllers/rooms_controllers.py,1/8/18c89df0871bed3f0a8bb9356b96a82d76cce07a
 D
 api/routes/admins.py,7/6/76163705cb4c5e30c15642cf480b9ca76a1cc306
-@
-api/db/models.py,4/0/402063606162c58bc5d24a24d989b73f61bf20bd
 U
 %api/controllers/admins_controllers.py,1/7/17fb878d1dd05a4f0c0c107801345f5d0327b9c4
 O
@@ -59,9 +53,13 @@ M
 api/services/StatusService.py,0/b/0b3eac52ccd107db79961aebf637bad0f30b3eeb
 L
 api/services/RoomsService.py,5/a/5a676d17bd9354bfe0ffe9e14c6a240481c42a63
-Q
-!api/services/CategoriesService.py,8/c/8c10f20fe87dfbbee8f1a35b0af0cbb934e190e2
 L
 api/services/AdminService.py,4/a/4a15e98a41190822d7ea64a2a5ac69fffb87a0ee
 W
 'api/controllers/bookings_controllers.py,e/b/eb3aec6ca9b2dfe8a5e3b79901d161ee2a27f096
+H
+api/routes/categories.py,b/4/b436e99314c0af36df3bcf35d367ae268f0c03b3
+Q
+!api/services/CategoriesService.py,8/c/8c10f20fe87dfbbee8f1a35b0af0cbb934e190e2
+@
+api/db/models.py,4/0/402063606162c58bc5d24a24d989b73f61bf20bd
--- api/services/BookingsService.py
+++ api/services/BookingsService.py
@@ -1,4 +1,5 @@
-from api.db.models import Bookings, db
+from api.db.models import Bookings, Rooms, Status, db
+from api.db.marshmallows import StatusSchema
 from sqlalchemy import and_, func
 
 
@@ -18,7 +19,11 @@ class BookingsService:
     def get_all_bookings(self):
         try:
             if self.check_in_date is not None:
-                bookings = Bookings.query.filter(func.date(Bookings.check_in_date) == func.date(self.check_in_date))
+                day_bookings = Bookings.query\
+                    .filter(func.date(Bookings.check_in_date) == func.date(self.check_in_date)).all()
+                checked = Bookings.query.filter_by(status_id=3).all()
+                bookings = day_bookings + checked
+
             elif self.booking_id is not None:
                 bookings = Bookings.query.filter_by(id=self.booking_id).all()
             elif self.last_name is not None:
@@ -62,10 +67,24 @@ class BookingsService:
             return {"ok": False, "error": error}
 
     def update_booking(self):
-        guests = self.number_of_guests if self.number_of_guests.isnumeric() else None
-        price = self.price_per_night if self.price_per_night.isnumeric() else None
+        guests = self.number_of_guests if type(self.number_of_guests) == int else None
+        price = self.price_per_night if type(self.price_per_night) == int else None
         status = self.status_id if type(self.status_id) == int else 1
         room = self.room_id if type(self.room_id) == int else None
+        get_status = {}
+
+        if status is not None:
+            status_schema = StatusSchema()
+            filter_status = Status.query.filter_by(id=status).first()
+            get_status = status_schema.dump(filter_status)
+
+        if get_status["booking_status"] == "Checked In" and room is not None:
+            room_occupancy = Rooms.query.filter_by(id=self.room_id).first()
+            room_occupancy.occupancy = 1
+
+        if get_status["booking_status"] == "Checked Out":
+            room_occupancy = Rooms.query.filter_by(id=self.room_id).first()
+            room_occupancy.occupancy = 0
 
         try:
             booking = Bookings.query.filter_by(id=self.booking_id).first()
